efb8896d2e9186c9c054d78962c200202cab0356
JonathaCB <97686758+JonathaCB@users.noreply.github.com>
2022-01-13 12:54:39 -0300

    Create dados.js

---
+++ dados.js
@@ -0,0 +1,29 @@
+function rolar() {
+    dado_1 = math.round(math.random() * 6);
+    dado_2 = math.round(math.random() * 6);
+
+    var resultado = dado_1 + dado_2; 
+    return resultado;
+}
+
+
+switch(rolar){
+    case 1:
+        console.log(/*mostrar a face correspondent*/);
+        break;
+    case 2:
+        console.log(/*mostrar a face correspondent*/)
+        break;
+    case 3:
+        console.log(/*mostrar a face correspondent*/)
+        break;
+    case 4:
+        console.log(/*mostrar a face correspondent*/)
+        break;
+    case 5:
+        console.log(/*mostrar a face correspondent*/)
+        break;
+    case 6:
+        console.log(/*mostrar a face correspondent*/)
+        break;
+}
